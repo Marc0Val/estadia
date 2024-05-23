beb67a30c4a03e985bd3d709579650f783fab2ba
Marco Valdez <mv9442422@gmail.com>
2024-05-22 21:20:15 -0600

    Actualizar componente TablaInfo.js y eliminar código comentado

--- frontend/src/components/TablaInfo.js
+++ frontend/src/components/TablaInfo.js
@@ -65,6 +65,51 @@
 
 // export default TablaInfo;
 
+// import React from "react";
+
+// const TablaInfo = ({ columns, data, totalRecords }) => {
+//   return (
+//     <div className="table-responsive">
+//       <table className="table align-middle table-hover">
+//         <thead>
+//           <tr>
+//             {totalRecords > 0 ? (
+//               <th colSpan={columns.length}>
+//                 Total de registros: {totalRecords}
+//               </th>
+//             ) : (
+//               <th colSpan={columns.length}></th>
+//             )}
+//           </tr>
+//           <tr>
+//             {columns.map((columnName, index) => (
+//               <th key={index}>{columnName}</th>
+//             ))}
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {totalRecords > 0 ? (
+//             data.map((row, rowIndex) => (
+//               <tr key={rowIndex}>
+//                 {columns.map((columnName, colIndex) => (
+//                   <td key={colIndex}>{row[columnName]}</td>
+//                 ))}
+//               </tr>
+//             ))
+//           ) : (
+//             <tr>
+//               <td colSpan={columns.length} className="text-center">
+//                 <strong>Ningún dato disponible en esta tabla</strong>
+//               </td>
+//             </tr>
+//           )}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+// export default TablaInfo;
 import React from "react";
 
 const TablaInfo = ({ columns, data, totalRecords }) => {
