0d46ad97249b0e529689422a799145b15c5210ef
Marco Valdez <mv9442422@gmail.com>
2024-06-08 23:14:35 -0600

    Agregar componentes para generar PDF y XLS de la tabla

--- frontend/src/components/Buttons/BotonPDF.js
+++ frontend/src/components/Buttons/BotonPDF.js
@@ -1,10 +1,3 @@
-// crear un componente que renderice un boton al hacer click genere un pdf con la informacion de la tabla
-// el pdf debe contener la informacion de la tabla
-// el boton recibira las propiedades de "a que tabla se le genera el pdf"
-// como estetica, el boton tiene que ser un boton de color gris con el icono de pdf
-// el boton debe tener un tooltip que diga "Generar PDF"
-// por el momento solo crear el boton con la estetica dicha, no es necesario que genere el pdf sino una alerta swal
-
 import React from "react";
 import Swal from "sweetalert2";
 
--- frontend/src/components/Buttons/BotonXLS.js
+++ frontend/src/components/Buttons/BotonXLS.js
@@ -1,10 +1,3 @@
-// crear un componente que renderice un boton al hacer click genere un xls con la informacion de la tabla
-// el xls debe contener la informacion de la tabla
-// el boton recibira las propiedades de "a que tabla se le genera el xls"
-// como estetica, el boton tiene que ser un boton de color gris con el icono de XLS
-// el boton debe tener un tooltip que diga "Generar XLS"
-// por el momento solo crear el boton con la estetica dicha, no es necesario que genere el xls sino una alerta swal
-
 import React from "react";
 import Swal from "sweetalert2";
 
