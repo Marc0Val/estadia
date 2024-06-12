f460a2e84b8897a91dfd5a9219791ff072392fdd
GaelGarcia02 <gael_3041230001@udt.edu.mx>
2024-06-12 00:53:52 -0600

    R

--- backend/routes/clients.routes.js
+++ backend/routes/clients.routes.js
@@ -19,7 +19,7 @@ router.get("/clients", getClients);
 //* Get one client
 router.get("/clients/:id", getClient);
 
-//* CReate new client
+//* Create new client
 router.post(
   "/clients",
   [validateEmail("email"), validateEmail("contact_email"), validateRequest],
