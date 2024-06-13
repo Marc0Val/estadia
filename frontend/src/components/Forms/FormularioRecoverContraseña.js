ad66a63b975f05f5fb4378ccd3da563d6e19150f
Marco Valdez <mv9442422@gmail.com>
2024-06-13 14:52:37 -0600

    Agregar validación de correo electrónico en el formulario de recuperación de contraseña

--- frontend/src/components/Forms/FormularioRecoverContraseña.js
+++ frontend/src/components/Forms/FormularioRecoverContraseña.js
@@ -1,29 +1,45 @@
 import React, { useState } from "react";
 import { Form, Button, Container } from "react-bootstrap";
+import Swal from "sweetalert2"
+import logo from "../../assets/logo.png";
 
 const FormularioRecoverContraseña = () => {
   const [email, setEmail] = useState("");
+  const [confirmEmail, setConfirmEmail] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Validar y enviar los datos del formulario.
-    console.log("Correo enviado:", email);
+    if (email === confirmEmail) {
+      Swal.fire({
+        title: "Se enviará un correo",
+        text: "Se enviará un correo si existe una cuenta asociada a este email",
+        icon: "info",
+        confirmButtonText: "OK",
+      });
+    } else {
+      Swal.fire({
+        title: "Error",
+        text: "Los correos electrónicos no coinciden",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+    }
   };
 
   const handleReset = () => {
     setEmail("");
+    setConfirmEmail("");
   };
+
   return (
     <Container className="mt-4">
-        <div className="mb-3">
-            <strong>Ingrese su correo para recuperar su contraseña</strong>
-
-        </div>
+      <div className="text-center mb-4">
+        <img src={logo} alt="Logo" style={{ width: "80px" }} />
+        <hr />
+      </div>
       <Form onSubmit={handleSubmit} onReset={handleReset}>
         <Form.Group controlId="formEmail" className="mb-3">
-          <Form.Label>
-            <strong>Correo *</strong>
-          </Form.Label>
+          <Form.Label>Correo Electrónico *</Form.Label>
           <Form.Control
             type="email"
             name="email"
@@ -32,10 +48,22 @@ const FormularioRecoverContraseña = () => {
             required
           />
         </Form.Group>
+
+        <Form.Group controlId="formConfirmEmail" className="mb-3">
+          <Form.Label>Confirmar Correo Electrónico *</Form.Label>
+          <Form.Control
+            type="email"
+            name="confirmEmail"
+            value={confirmEmail}
+            onChange={(e) => setConfirmEmail(e.target.value)}
+            required
+          />
+        </Form.Group>
+
         <Button
           variant="success"
           type="submit"
-          disabled={email === "" ? true : false}
+          disabled={email === "" || confirmEmail === "" || email !== confirmEmail}
         >
           <i className="fas fa-save"></i> Enviar
         </Button>
