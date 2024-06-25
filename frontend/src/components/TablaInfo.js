7f8715807ebda3d13533861427b99328c27bec78
Marco Valdez <mv9442422@gmail.com>
2024-06-25 03:19:57 -0600

    Agregar funcionalidad para ocultar columnas en TablaInfo

--- frontend/src/components/TablaInfo.js
+++ frontend/src/components/TablaInfo.js
@@ -1,27 +1,39 @@
 import React from "react";
 import BotonEditarModal from "./Buttons/BotonEditarModal";
 
-const TablaInfo = ({ columns, data, totalRecords, fetchElemento }) => {
+const TablaInfo = ({
+  columns,
+  data,
+  totalRecords,
+  fetchElemento,
+  hiddenColumns = [],
+  customColumnNames = {},
+}) => {
   return (
     <div className="table-responsive">
       <table className="table align-middle table-hover">
         <thead>
           <tr>
             {totalRecords > 0 ? (
-              <th colSpan={columns.length + 1} className="text-center">
+              <th
+                colSpan={Object.keys(customColumnNames).length + 1}
+                className="text-center"
+              >
                 Total de registros: {totalRecords}
               </th>
             ) : (
-              <th colSpan={columns.length + 1}></th>
+              <th colSpan={Object.keys(customColumnNames).length + 1}></th>
             )}
           </tr>
           <tr>
             {columns.map((columnName, index) =>
-              columnName.startsWith("id_") ? null : (
-                <th key={index}>{columnName}</th>
+              hiddenColumns.includes(columnName) ? null : (
+                <th key={index}>
+                  {customColumnNames[columnName] || columnName}
+                </th>
               )
             )}
-            <th className="text-center">Accion</th>
+            <th className="text-center">Acción</th>
           </tr>
         </thead>
         <tbody>
@@ -29,7 +41,7 @@ const TablaInfo = ({ columns, data, totalRecords, fetchElemento }) => {
             data.map((row, rowIndex) => (
               <tr key={rowIndex} className="table-row">
                 {columns.map((columnName, colIndex) =>
-                  columnName.startsWith("id_") ? null : (
+                  hiddenColumns.includes(columnName) ? null : (
                     <td key={colIndex}>{row[columnName]}</td>
                   )
                 )}
@@ -44,7 +56,10 @@ const TablaInfo = ({ columns, data, totalRecords, fetchElemento }) => {
             ))
           ) : (
             <tr>
-              <td colSpan={columns.length + 1} className="text-center">
+              <td
+                colSpan={Object.keys(customColumnNames).length + 1}
+                className="text-center"
+              >
                 <strong>Ningún dato disponible en esta tabla</strong>
               </td>
             </tr>
