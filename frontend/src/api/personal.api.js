c7ceff1e8c84eca0a1bd7f7ea6500d72ee1494b6
GaelGarcia02 <gael_3041230001@udt.edu.mx>
2024-07-19 15:31:34 -0600

    coso

--- frontend/src/api/personal.api.js
+++ frontend/src/api/personal.api.js
@@ -1,11 +1,23 @@
-//TODO
 import axios from "axios";
 
-export const getPersonalRequest = async () =>
-  await axios.get("http://127.0.01:4000/api/personal");
+const route = "http://127.0.01:4000/api";
+
+export const getAllPersonalRequest = async () =>
+  await axios.get(`${route}/personal`);
+
+export const getPersonalRequest = async (id) =>
+  await axios.get(`${route}/personal/${id}`);
 
 export const getPersonalRoleRequest = async () =>
   await axios.get("http://127.0.01:4000/api/personal-role");
 
-export const createPersonalRequest = async (usuario) =>
-  await axios.post("http://127.0.01:4000/api/personal", usuario);
+export const createPersonalRequest = async (personal) => {
+  console.log("Datos enviados:", personal);
+  return await axios.post(`${route}/personal`, personal);
+};
+
+export const updatePersonalRequest = async (id, personal) =>
+  await axios.put(`${route}/personal/${id}`, personal);
+
+export const deletePersonalRequest = async (id) =>
+  await axios.delete(`${route}/personal/${id}`);
